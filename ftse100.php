e4d100ec1dbe33dc1aae48084ccfc56cc2b9da8e
KiwiCoder <KiwiCoder@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-04 10:21:57 +0000

    first commit
    git-svn-id: https://plugins.svn.wordpress.org/ftse-chart/trunk@1019429 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ ftse100.php
@@ -0,0 +1,60 @@
+<?php
+/*
+  Plugin Name: FTSE 100 Chart
+  Plugin URI:
+  Description: Adds a Widget so you can embed a FTSE 100 chart anywhere on your Wordpress site.
+  Version: 1.0
+  Author: KiwiCoder
+  Author URI: http://coder.kiwi
+  License: GPLv2
+ */
+
+class ftse_widget extends WP_Widget {
+
+    function ftse_widget() {
+        $widget_ops = array('classname' => 'ftse_widget', 'description' => __('FTSE 100 Widget', 'ftse-plugin'));
+        $this->WP_Widget('ftse_widget_css', __('FTSE Widget', 'ftse-plugin'), $widget_ops);
+    }
+
+    function form($instance) {
+        // admin form
+        $defaults = array('title' => __('FTSE 100 from Yahoo! Finance', 'ftse-plugin'));
+        $instance = wp_parse_args((array) $instance, $defaults);
+        $title = strip_tags($instance['title']);
+        ?>
+        <p><?php _e('Title', 'ftse-plugin') ?>: <input class="widefat" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
+
+        <?php
+    }
+
+    function update($new_instance, $old_instance) {
+        // save widget options
+        $instance = $old_instance;
+        $instance['title'] = strip_tags($new_instance['title']);
+        return $instance;
+    }
+
+    function widget($args, $instance) {
+        // display the widget
+        extract($args);
+        echo $before_widget;
+        $title = apply_filters('widget_title', $instance['title']);
+        if (!empty($title)) {
+            echo $before_title . $title . $after_title;
+        };
+        $json = file_get_contents('http://m8y.co/api/ftse');
+        $obj = json_decode($json);
+
+        echo '<img src="' . $obj->url . '"/>';
+
+        echo $after_widget;
+    }
+
+}
+
+function ftse_register_widget() {
+    register_widget('ftse_widget');
+}
+
+add_action('widgets_init', 'ftse_register_widget');
+?>
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== FTSE 100 Chart ===
+Contributors: KiwiCoder
+Donate link: 
+Tags: widget,FTSE, UKX, FTSE100
+Requires at least: 3.0
+Tested up to: 3.1
+Stable tag: 1.0
+FTSE 100 chart widget.
+License: GPLv2
+== Description ==
+Adds a Widget so you can easily embed a FTSE 100 chart anywhere 
+on your Wordpress site.
+== Installation ==
+1. Upload the FTSE100 folder to the '/wp-content/plugins/' folder
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Add the widget to a panel on your site via the Wordpress Widgets menu
+== Frequently Asked Questions ==
+
+= Where does the chart come from? =
+Yahoo! Finance is polled at intervals for the current chart
+= How often is the chart updated? =
+At least once per hour, more often if the server is not overloaded
+= Why not embed the Yahoo! Finance directly = 
+You can of course do this if you prefer. You might choose to use this widget for 
+greater control and to take advantage of chart caching.
+== Screenshots ==
+1. FTSE 100 Chart from Yahoo! Finance.
+== Changelog ==
+= Version 1.0 =
+* First version
+== Upgrade Notice ==
+n/a
